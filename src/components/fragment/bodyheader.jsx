7332ce625896c8419a8dea8f7e2bc1c100491444
JS <zks145@naver.com>
2024-03-02 18:02:30 +0900

    공장 페이지 UI 수정

--- src/components/fragment/bodyheader.jsx
+++ src/components/fragment/bodyheader.jsx
@@ -1,21 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useUser } from '../../UserContext';
 
 function Header() {
+  const [showMenu, setShowMenu] = useState(false);
   const { user } = useUser(); 
-
   const username = user?.name;
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="header" style={{display: 'flex', justifyContent: 'space-between', alignItems: 'center'}}>
       <Link to="/home">
         <h3 className="text-muted">PRODUCT HUB</h3>
       </Link>
-      <p style={{marginRight: '10px', marginTop: '10px'}}>
+      <div onClick={toggleMenu} style={{position: 'relative', marginRight: '10px', marginTop: '10px', cursor: 'pointer'}}>
         사용자: {username}
-      </p>
-      {/* <button className='div-header-btn' type="submit">로그아웃</button> */}
+        {showMenu && (
+          <div style={{
+            position: 'absolute',
+            width: '200px',
+            height: '80px',
+            backgroundColor: 'white',
+            boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+            marginTop: '10px',
+            right: 0,
+            alignItems: 'center',
+            display: 'flex',
+            justifyContent: 'space-between'
+          }}>
+            {/* <Link to="/change-password" className="menu-item">비밀번호 변경</Link> */}
+            <button className='div-header-btn' type="submit">로그아웃</button>
+          </div>
+        )}
+      </div>      
     </div>
   );
 }
